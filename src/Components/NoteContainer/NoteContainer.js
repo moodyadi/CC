bee6dadecefddc85f21d5bed2f3fbe46fd452fef
moodyadi <adityachavan7@outlook.com>
2023-12-13 03:32:26 +0530

    shifted copy buttons to left from right

--- src/Components/NoteContainer/NoteContainer.js
+++ src/Components/NoteContainer/NoteContainer.js
@@ -25,7 +25,7 @@ function NoteContainer(props) {
 
   return (
     <div className="note-container">
-      <h2>Notes</h2>
+      <h2>CopyCat</h2>
       <div className="note-container_notes custom-scroll">
         {notes?.length > 0 ? (
           notes.map((item) => (
@@ -38,7 +38,7 @@ function NoteContainer(props) {
             />
           ))
         ) : (
-          <h3>No Notes present</h3>
+          <h3>No Credentials added</h3>
         )}
       </div>
     </div>
